3115018fa6591fe792da6330e1f4d62970a4b593
Marek Jasiński <91marek@gmail.com>
2012-04-23 15:19:23 +0200

    DLMlib

---
+++ DLM-via-FIFO/DLMlib.c
@@ -0,0 +1,70 @@
+/*
+ * DLMlib.c
+ */
+
+#include "DLMlib.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define DLM_PATH "/tmp/DLM/"
+#define DLM_FIFO_PATH "/tmp/DLM/DLMfifo"
+
+int DLM_lock(int resource_id, int lock_type, long timeout) {
+	DLMrequest request = { getpid(), resource_id, lock_type, timeout };
+	DLMresponse response;
+	int dlmfifo;
+	int clientfifo;
+	char path[32];
+
+	sprintf(path, "%s%d", DLM_PATH, request.pid);
+	if ((dlmfifo = open(DLM_FIFO_PATH, O_WRONLY)) < 0) {
+		return EOPENDLMFIFO;
+	}
+	if (mkfifo(path, S_IRUSR | S_IWUSR) < 0) {
+		close(dlmfifo);
+		return ECREATEFIFO;
+	}
+	if ((clientfifo = open(path, O_RDONLY))) {
+		close(dlmfifo);
+		unlink(path);
+		return EOPENCLIENTFIFO;
+	}
+	if (write(dlmfifo, &request, sizeof(request)) != sizeof(request) ) {
+		close(dlmfifo);
+		close(clientfifo);
+		unlink(path);
+		return EWRITE;
+	}
+	if (read(clientfifo, &response, sizeof(response)) != sizeof(response)) {
+		// To nie powinno sie zdarzyc
+		close(dlmfifo);
+		close(clientfifo);
+		unlink(path);
+		return EREAD;
+	}
+	close(dlmfifo);
+	close(clientfifo);
+	unlink(path);
+	return response.response;
+}
+
+int DLM_unlock( int resource_id ) {
+	DLMrequest request = { getpid(), resource_id, FREERESOURCE, 0 };
+	int dlmfifo;
+
+	if ((dlmfifo = open(DLM_FIFO_PATH, O_WRONLY)) < 0) {
+		return EOPENDLMFIFO;
+	}
+	if (write(dlmfifo, &request, sizeof(request)) != sizeof(request) ) {
+		close(dlmfifo);
+		return EWRITE;
+	}
+	close(dlmfifo);
+	return 0;
+}
+
+int DLM_trylock( int resource_id, int lock_type ) {
+	return DLM_lock(resource_id, lock_type, -1);
+}
---
+++ DLM-via-FIFO/DLMlib.h
@@ -0,0 +1,33 @@
+/*
+ * DLMlib.h
+ */
+
+#ifndef DLMLIB_H_
+#define DLMLIB_H_
+
+#include <unistd.h>
+
+#define FREERESOURCE -1
+
+#define EOPENDLMFIFO -1
+#define ECREATEFIFO -2
+#define EOPENCLIENTFIFO -3
+#define EWRITE -4
+#define EREAD -5
+
+typedef struct DLMrequest {
+	pid_t pid;
+	int resource_id;
+	int lock_type;
+	long timeout;
+} DLMrequest;
+
+typedef struct DLMresponse {
+	int response;
+} DLMresponse;
+
+int DLM_lock(int resource_id, int lock_type, long timeout);
+int DLM_unlock( int resource_id );
+int DLM_trylock( int resource_id, int lock_type );
+
+#endif /* DLMLIB_H_ */
