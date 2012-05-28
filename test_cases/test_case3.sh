fbb0fe2bdff369f3afa1813ef15ecd89d0e9a533
marcin <marcin@vm-debian.lan>
2012-05-28 14:40:06 +0200

    Poprawka man dla DLM-client i testcase

--- test_cases/test_case3.sh
+++ test_cases/test_case3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd ../bin
 # resource_id lock_type timeout(ms) sleep_time(s) iterations
-echo "testowanie funkcji try_lock"
+echo "testowanie funkcji try_lock testcase3"
 ./DLM-client 100 1 0 5 3 &
 arr[0]=$!
-./DLM-client 100 1 -2 5 3 &
+./DLM-client 100 1 -2 4 2 &
 arr[1]=$!
 for pid in $arr
 do
